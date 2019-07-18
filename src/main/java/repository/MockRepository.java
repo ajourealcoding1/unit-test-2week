0fb87568753f18941ab8129b17c2743fa62e9053
rkdcksgur <rkdcksgur@ajou.ac.kr>
2019-07-19 01:13:18 +0900

    modify mockrepository interface to class and modify findall, findbyname method

--- src/main/java/repository/MockRepository.java
+++ src/main/java/repository/MockRepository.java
@@ -4,13 +4,30 @@ import domain.ConvenienceStoreItem;
 
 import java.util.List;
 
-public interface MockRepository {
-    List<ConvenienceStoreItem> findAll();
+public class MockRepository {
 
-    ConvenienceStoreItem findByName(String name);
+    List<ConvenienceStoreItem> convenienceStoreItems = null;
 
-    ConvenienceStoreItem updateCategoryByName(String name, String category);
-    ConvenienceStoreItem updatePriceByName(String name, int price);
+    public List<ConvenienceStoreItem> findAll(){
+        return convenienceStoreItems;
+    }
 
-    void addConvenienceStoreItem(ConvenienceStoreItem convenienceStoreItem);
+    public ConvenienceStoreItem findByName(String name){
+        for(int i=0; i < convenienceStoreItems.size(); i++) {
+            if (convenienceStoreItems.get(i).getName().equals(name))
+                return convenienceStoreItems.get(i);
+        }
+        return null;
+    }
+
+    public ConvenienceStoreItem updateCategoryByName(String name, String category){
+
+        return null;
+    }
+
+    public ConvenienceStoreItem updatePriceByName(String name, int price){
+        return null;
+    }
+
+    public void addConvenienceStoreItem(ConvenienceStoreItem convenienceStoreItem){}
 }
