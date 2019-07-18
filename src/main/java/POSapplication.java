84e2f9b14739ab8d7ac2c400f7959d143fd69305
rkdcksgur <rkdcksgur@ajou.ac.kr>
2019-07-19 00:53:29 +0900

    add description in main class

--- src/main/java/POSapplication.java
+++ src/main/java/POSapplication.java
@@ -1,6 +1,7 @@
 public class POSapplication {
 
     public static void main(String[] args){
-
+        System.out.println("------------------- POS application -------------------\n");
+        System.out.println("It stores the list of item and calculates total price.\n");
     }
 }
